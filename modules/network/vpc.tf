24dc5e7b27b18dc6c16868474fb5c63bf045ce5e
aman-deep-dev <aman.deep307dev@gmail.com>
2024-05-04 11:56:37 +0530

    format vpc.tf

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -2,9 +2,9 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.8.1"
 
-  name    = local.vpc_name
-  cidr = var.vpc_cidr
-  azs = var.vpc_azs
+  name            = local.vpc_name
+  cidr            = var.vpc_cidr
+  azs             = var.vpc_azs
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
